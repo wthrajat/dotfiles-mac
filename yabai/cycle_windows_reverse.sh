8fcba5918f6237594172d61ea09827b03ef55f13
wthrajat <rajatkhanduri290102@gmail.com>
2024-12-01 00:25:07 +0530

    let there be configs

---
+++ yabai/cycle_windows_reverse.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+win=$(yabai -m query --windows --window last | jq '.id')
+while : ; do
+    yabai -m window $win --swap prev &> /dev/null
+    if [[ $? -eq 1 ]]; then
+        break
+    fi
+done
