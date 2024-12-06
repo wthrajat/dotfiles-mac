c81cb22a446329bb576ada7af256caf08c153407
wthrajat <rajatkhanduri290102@gmail.com>
2024-12-06 12:11:03 +0530

    feat: add nvim, fishrc changes,  yabai changes

---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,406 @@
+local util = require("util")
+
+return {
+  -- nerd font supported icons
+  {
+    "nvim-tree/nvim-web-devicons",
+    lazy = true,
+  },
+
+  -- status line
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    dependencies = { "AndreM222/copilot-lualine" },
+    commit = "640260d7c2d98779cab89b1e7088ab14ea354a02", -- i have some issue with latest commit (delay between mode switching)
+    config = function()
+      require("lualine").setup({
+        options = {
+          globalstatus = true,
+          disabled_filetypes = {
+            statusline = { "alpha" },
+            winbar = {},
+          },
+          component_separators = { left = "", right = "" },
+          section_separators = { left = "", right = "" },
+          refresh = {
+            statusline = 500,
+          },
+        },
+        sections = require("util.statusline").sections,
+        extensions = {
+          "neo-tree",
+          "nvim-dap-ui",
+        },
+      })
+
+      vim.opt.showmode = false
+    end,
+  },
+
+  -- color highlighter
+  {
+    "NvChad/nvim-colorizer.lua",
+    event = "BufEnter",
+    config = function()
+      require("colorizer").setup({
+        filetypes = { "*" },
+        user_default_options = {
+          names = false,
+          tailwind = "both",
+          mode = "background",
+        },
+      })
+    end,
+  },
+
+  -- git decorations
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "BufEnter",
+    config = function()
+      require("gitsigns").setup({
+        signs = {
+          add = { text = "│" },
+          change = { text = "│" },
+        },
+        current_line_blame = false,
+        current_line_blame_opts = {
+          delay = 200,
+        },
+        -- signs = {
+        --   add = { text = '+' },
+        --   change = { text = '~' },
+        --   delete = { text = '_' },
+        --   topdelete = { text = '‾' },
+        --   changedelete = { text = '~' },
+        -- },
+      })
+    end,
+  },
+
+  -- Standalone UI for nvim-lsp progress
+  {
+    "j-hui/fidget.nvim",
+    event = "LspAttach",
+    config = function()
+      require("fidget").setup({
+        -- text = {
+        --   spinner = "meter",
+        -- },
+        -- window = {
+        --   blend = 0, -- set 0 if using transparent background, otherwise set 100
+        -- },
+        progress = {
+          poll_rate = 200,
+          ignore_done_already = true,
+          display = {
+            done_ttl = 0.5,
+            -- done_icon = " ",
+            -- Icon shown when LSP progress tasks are in progress
+            progress_icon = { pattern = "meter", period = 1 },
+            -- Highlight group for in-progress LSP tasks
+            progress_style = "WarningMsg",
+            group_style = "WarningMsg", -- Highlight group for group name (LSP server name)
+            icon_style = "WarningMsg", -- Highlight group for group icons
+            done_style = "Conditional", -- Highlight group for completed LSP tasks
+          },
+        },
+        notification = {
+          -- override_vim_notify = true,
+          window = {
+            winblend = 0,
+          },
+        },
+      })
+    end,
+  },
+
+  -- Neovim plugin to improve the default vim.ui interfaces
+  {
+    "stevearc/dressing.nvim",
+    event = "BufEnter",
+    config = function()
+      require("dressing").setup({
+        input = {
+          win_options = {
+            winblend = 0,
+          },
+        },
+      })
+    end,
+  },
+
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+    cmd = "Neotree",
+    keys = {
+      { "<C-b>", "<cmd>Neotree toggle<cr>", mode = "n", desc = "Toggle Neotree" },
+    },
+    config = function()
+      require("neo-tree").setup({
+        close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+        filesystem = {
+          follow_current_file = {
+            enabled = true,
+          },
+          filtered_items = {
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            hide_hidden = false,
+            hide_by_name = {
+              ".git",
+            },
+          },
+        },
+      })
+    end,
+  },
+
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    version = "2",
+    config = function()
+      vim.api.nvim_set_hl(0, "IndentBlanklineContextChar", { link = "IndentBlanklineChar" })
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "*",
+        callback = function()
+          vim.api.nvim_set_hl(0, "IndentBlanklineContextChar", { link = "IndentBlanklineChar" })
+        end,
+        group = vim.api.nvim_create_augroup("RelinkIndentBlanklineHightLightGroup", { clear = true }),
+        desc = "Relink IndentBlankline Highlight Group",
+      })
+      require("indent_blankline").setup({
+        char = "",
+        context_char = "│",
+        show_current_context = true,
+      })
+    end,
+  },
+
+  {
+    "luukvbaal/statuscol.nvim",
+    config = function()
+      local builtin = require("statuscol.builtin")
+
+      local function get_num_wraps()
+        -- Calculate the actual buffer width, accounting for splits, number columns, and other padding
+        local wrapped_lines = vim.api.nvim_win_call(0, function()
+          local winid = vim.api.nvim_get_current_win()
+
+          -- get the width of the buffer
+          local winwidth = vim.api.nvim_win_get_width(winid)
+          local numberwidth = vim.wo.number and vim.wo.numberwidth or 0
+          local signwidth = vim.fn.exists("*sign_define") == 1 and vim.fn.sign_getdefined() and 2 or 0
+          local foldwidth = vim.wo.foldcolumn or 0
+
+          -- subtract the number of empty spaces in your statuscol. I have
+          -- four extra spaces in mine, to enhance readability for me
+          local bufferwidth = winwidth - numberwidth - signwidth - foldwidth - 4
+
+          -- fetch the line and calculate its display width
+          local line = vim.fn.getline(vim.v.lnum)
+          local line_length = vim.fn.strdisplaywidth(line)
+
+          return math.floor(line_length / bufferwidth)
+        end)
+
+        return wrapped_lines
+      end
+
+      require("statuscol").setup({
+        relculright = true, -- whether to right-align the cursor line number with 'relativenumber' set
+        ft_ignore = { "alpha", "neo-tree", "oil" },
+        segments = {
+          { sign = { name = { "Diagnostic" } } },
+          { sign = { name = { "Dap.*" } }, click = "v:lua.ScLa" },
+          { -- line number
+            text = {
+              -- TODO: turn into absolute line number when in visual mode
+              function(args)
+                local v_hl = ""
+                local mode = vim.fn.strtrans(vim.fn.mode()):lower():gsub("%W", "")
+                if mode == "v" then
+                  local v_range = util.get_visual_range()
+                  local is_in_range = vim.v.lnum >= v_range[1] and vim.v.lnum <= v_range[3]
+                  v_hl = is_in_range and "%#CursorLineNr#" or ""
+                end
+
+                if vim.v.virtnum < 0 then
+                  return "-"
+                elseif vim.v.virtnum > 0 and (vim.wo.number or vim.wo.relativenumber) then
+                  local num_wraps = get_num_wraps()
+
+                  if vim.v.virtnum == num_wraps then
+                    return v_hl .. "%=" .. "└"
+                  else
+                    return v_hl .. "%=" .. "├"
+                  end
+                end
+
+                return v_hl .. builtin.lnumfunc(args)
+              end,
+            },
+            click = "v:lua.ScLa",
+          },
+          { sign = { namespace = { "gitsign" }, auto = false } },
+        },
+      })
+    end,
+  },
+
+  {
+    "echasnovski/mini.animate",
+    version = false,
+    cmd = "CoworkingToggle",
+    config = function()
+      local isCoworking = false
+      local animate = require("mini.animate")
+      local Util = require("lazy.core.util")
+
+      local coworking_setup = function()
+        if isCoworking then
+          animate.setup({
+            cursor = { enable = false },
+            scroll = {
+              enable = true,
+              timing = animate.gen_timing.cubic({ duration = 50, unit = "total" }),
+            },
+            resize = { enable = false },
+            open = { enable = false },
+            close = { enable = false },
+          })
+
+          vim.keymap.set("n", "<C-d>", "<C-d>")
+          vim.keymap.set("n", "<C-u>", "<C-u>")
+
+          vim.opt.relativenumber = false
+          vim.opt.number = true
+        else
+          animate.setup({
+            cursor = { enable = false },
+            scroll = { enable = false },
+            resize = { enable = false },
+            open = { enable = false },
+            close = { enable = false },
+          })
+
+          vim.keymap.set("n", "<C-d>", "<C-d>zz")
+          vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+          vim.opt.relativenumber = true
+          vim.opt.number = true
+        end
+      end
+
+      vim.api.nvim_create_user_command("CoworkingToggle", function()
+        isCoworking = not isCoworking
+        if isCoworking then
+          Util.warn("Enabled coworking mode", { title = "Coworking" })
+        else
+          Util.info("Disabled coworking mode", { title = "Coworking" })
+        end
+        coworking_setup()
+      end, { desc = "Toggle coworking mode (add scrolling animation)" })
+
+      coworking_setup()
+    end,
+  },
+
+  -- a lua powered greeter like vim-startify / dashboard-nvim
+  {
+    "goolord/alpha-nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      local dashboard = require("alpha.themes.dashboard")
+
+      -- local logo = {
+      --   [[0    0    0    0    0    0]],
+      --   [[1    1    1    1    1    1]],
+      --   [[1    1    1    1    1    1]],
+      --   [[0    0    0    1    0    0]],
+      --   [[1    0    1    0    1    1]],
+      --   [[1    1    1    1    0    1]],
+      --   [[1    0    1    1    0    0]],
+      --   [[0    1    1    0    1    1]],
+      -- }
+      --
+      local version = "v" .. vim.version().major .. "." .. vim.version().minor .. "." .. vim.version().patch
+      dashboard.section.header.val = "NVIM " .. version
+
+      local userName = "Leon"
+      local greeting = util.get_greeting(userName)
+
+      local greetHeading = {
+        type = "text",
+        val = greeting,
+        opts = {
+          position = "center",
+          hl = "String",
+        },
+      }
+
+      dashboard.section.buttons.val = {
+        util.button("n", " " .. " New file", "<cmd> enew <cr>"),
+        util.button("ctrl + p", " " .. " Find file", "<cmd> Telescope find_files <cr>"),
+        util.button("q", " " .. " Quit", "<cmd> qa <cr>"),
+      }
+
+      dashboard.config.layout = {
+        { type = "padding", val = vim.fn.max({ 2, vim.fn.floor(vim.fn.winheight(0) * 0.35) }) },
+        dashboard.section.header,
+        { type = "padding", val = 1 },
+        greetHeading,
+        { type = "padding", val = 2 },
+        dashboard.section.buttons,
+        { type = "padding", val = 1 },
+        dashboard.section.footer,
+        { type = "padding", val = 100 },
+      }
+
+      for _, button in ipairs(dashboard.section.buttons.val) do
+        button.opts.hl = "AlphaButtons"
+        button.opts.hl_shortcut = "AlphaShortcut"
+      end
+      dashboard.section.header.opts.hl = "AlphaHeader"
+      dashboard.section.buttons.opts.hl = "AlphaButtons"
+      dashboard.section.footer.opts.hl = "AlphaFooter"
+
+      require("alpha").setup(dashboard.config)
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "LazyVimStarted",
+        desc = "Add load plugins greeting to Alpha dashboad",
+        once = true,
+        callback = function()
+          local stats = require("lazy").stats()
+          local ms = math.floor(stats.startuptime * 100 + 0.5) / 100
+          dashboard.section.footer.val = { "Neovim loaded  " .. stats.count .. " plugins in " .. ms .. "ms" }
+          pcall(vim.cmd.AlphaRedraw)
+        end,
+      })
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "AlphaReady",
+        desc = "Prevent from first ctrl-o not work when enter nvim",
+        once = true,
+        callback = function()
+          local jump_back_key = vim.api.nvim_replace_termcodes("<C-o>", true, false, true)
+          vim.api.nvim_feedkeys(jump_back_key, "n", false)
+        end,
+      })
+    end,
+  },
+  {
+    "nvzone/showkeys",
+    cmd = "ShowkeysToggle",
+    config = function()
+      require("showkeys").setup({
+        position = "bottom-left",
+      })
+    end,
+  },
+}
---
+++ nvim/lua/util/init.lua
@@ -0,0 +1,147 @@
+local M = {}
+
+---A helper function to find configurations
+---@param config_names string[] A list of config names you want to find (e.g. { ".stylua.toml", "stylua.toml" })
+---@param default_dir string Default config directory (e.g. vim.fn.stdpath("config") .. "/lua/plugins/formatting/configs/")
+---@return string config_dir The directory of first found configuration (project's config > default's config)
+function M.config_finder(config_names, default_dir)
+  -- prevent that user not provide last seperator
+  if string.sub(default_dir, string.len(default_dir)) ~= "/" then
+    default_dir = default_dir .. "/"
+  end
+
+  local config_dir = ""
+
+  -- search from project recursively
+  for _, name in ipairs(config_names) do
+    local found_root = require("lspconfig").util.root_pattern(name)(vim.loop.cwd())
+    if found_root then
+      config_dir = found_root .. "/" .. name
+      if _G.IS_WINDOWS then
+        config_dir = string.gsub(config_dir, "/", "\\\\")
+      end
+      return config_dir
+    end
+  end
+
+  -- search from defalut_dir
+  for _, name in ipairs(config_names) do
+    if vim.loop.fs_stat(default_dir .. name) then
+      config_dir = default_dir .. name
+      if _G.IS_WINDOWS then
+        config_dir = string.gsub(config_dir, "/", "\\\\")
+      end
+      return config_dir
+    end
+  end
+
+  return config_dir
+end
+
+--- Create a button entity to use with the alpha dashboard
+--- @param sc string The keybinding string to convert to a button
+--- @param txt string The explanation text of what the keybinding does
+--- @param keybind string? optional
+--- @param keybind_opts table? optional
+--- @return table # A button entity table for an alpha configuration
+function M.button(sc, txt, keybind, keybind_opts)
+  local sc_ = sc:gsub("%s", ""):gsub("SPC", "<leader>")
+
+  local opts = {
+    position = "center",
+    shortcut = sc,
+    cursor = 3,
+    width = 30,
+    align_shortcut = "right",
+    hl_shortcut = "Keyword",
+  }
+  if keybind then
+    keybind_opts = vim.F.if_nil(keybind_opts, { noremap = true, silent = true, nowait = true })
+    opts.keymap = { "n", sc_, keybind, keybind_opts }
+  end
+
+  local function on_press()
+    local key = vim.api.nvim_replace_termcodes(keybind or sc_ .. "<Ignore>", true, false, true)
+    vim.api.nvim_feedkeys(key, "t", false)
+  end
+
+  return {
+    type = "button",
+    val = txt,
+    on_press = on_press,
+    opts = opts,
+  }
+end
+
+--- Get greeting message based on current time and username
+--- @param name string Username
+function M.get_greeting(name)
+  local tableTime = os.date("*t")
+  local hour = tableTime.hour
+  local greetingsTable = {
+    [1] = "󰙃  Why are you still up, " .. name .. "?",
+    [2] = "  Good morning, " .. name,
+    [3] = "  Good afternoon, " .. name,
+    [4] = "󰖔  Good evening, " .. name,
+  }
+  local greetingIndex = 0
+  if hour >= 23 or hour < 7 then
+    greetingIndex = 1
+  elseif hour < 12 then
+    greetingIndex = 2
+  elseif hour >= 12 and hour < 18 then
+    greetingIndex = 3
+  elseif hour >= 18 and hour < 23 then
+    greetingIndex = 4
+  end
+  return greetingsTable[greetingIndex]
+end
+
+-- From: https://neovim.discourse.group/t/how-do-you-work-with-strings-with-multibyte-characters-in-lua/2437/4
+function M.char_byte_count(s, i)
+  if not s or s == "" then
+    return 1
+  end
+
+  local char = string.byte(s, i or 1)
+
+  -- Get byte count of unicode character (RFC 3629)
+  if char > 0 and char <= 127 then
+    return 1
+  elseif char >= 194 and char <= 223 then
+    return 2
+  elseif char >= 224 and char <= 239 then
+    return 3
+  elseif char >= 240 and char <= 244 then
+    return 4
+  end
+end
+
+function M.char_on_pos(pos)
+  pos = pos or vim.fn.getpos(".")
+  return tostring(vim.fn.getline(pos[1])):sub(pos[2], pos[2])
+end
+
+function M.get_visual_range()
+  local sr, sc = unpack(vim.fn.getpos("v"), 2, 3)
+  local er, ec = unpack(vim.fn.getpos("."), 2, 3)
+
+  -- To correct work with non-single byte chars
+  local byte_c = M.char_byte_count(M.char_on_pos({ er, ec }))
+  ec = ec + (byte_c - 1)
+
+  local range = {}
+
+  if sr == er then
+    local cols = sc >= ec and { ec, sc } or { sc, ec }
+    range = { sr, cols[1] - 1, er, cols[2] }
+  elseif sr > er then
+    range = { er, ec - 1, sr, sc }
+  else
+    range = { sr, sc - 1, er, ec }
+  end
+
+  return range
+end
+
+return M
