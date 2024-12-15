771c3657d3256093389ef8552fb1b0e655837a98
wthrajat <rajatkhanduri290102@gmail.com>
2024-12-15 14:33:00 +0530

    chore: minor changes

--- fish/config.fish
+++ fish/config.fish
@@ -3,5 +3,10 @@ if status is-interactive
 end
 
 set -x LANG en_US.UTF-8
+set -x KAFKAJS_NO_PARTITIONER_WARNING 1
 
 set PATH /Users/rajat/.nvm/versions/node/v23.3.0/bin $PATH
+set PATH /opt/homebrew/Caskroom/flameshot/12.1.0/flameshot.app/Contents/MacOS/ $PATH
+
+# Added by `rbenv init` on Fri Dec  6 23:13:12 IST 2024
+status --is-interactive; and rbenv init - --no-rehash fish | source
