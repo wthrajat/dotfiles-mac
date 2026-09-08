c5a71109c6ee0513fb1b3fd18fb5e781d8f1e177
wthrajat <rajatkhanduri290102@gmail.com>
2026-09-09 03:00:03 +0530

    feat(lazy): remove unused sidekick and language extras from configs

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,5 +1,4 @@
 return {
-  { import = "lazyvim.plugins.extras.ai.sidekick" },
   {
     "folke/sidekick.nvim",
     opts = {
--- nvim/lua/plugins/languages.lua
+++ nvim/lua/plugins/languages.lua
@@ -1,21 +1,4 @@
 return {
-  { import = "lazyvim.plugins.extras.lang.astro" },
-  { import = "lazyvim.plugins.extras.lang.clangd" },
-  { import = "lazyvim.plugins.extras.lang.docker" },
-  { import = "lazyvim.plugins.extras.lang.go" },
-  { import = "lazyvim.plugins.extras.lang.json" },
-  { import = "lazyvim.plugins.extras.lang.markdown" },
-  { import = "lazyvim.plugins.extras.lang.python" },
-  { import = "lazyvim.plugins.extras.lang.rust" },
-  { import = "lazyvim.plugins.extras.lang.solidity" },
-  { import = "lazyvim.plugins.extras.lang.sql" },
-  { import = "lazyvim.plugins.extras.lang.tailwind" },
-  { import = "lazyvim.plugins.extras.lang.terraform" },
-  { import = "lazyvim.plugins.extras.lang.toml" },
-  { import = "lazyvim.plugins.extras.lang.typescript" },
-  { import = "lazyvim.plugins.extras.lang.vue" },
-  { import = "lazyvim.plugins.extras.lang.yaml" },
-  { import = "lazyvim.plugins.extras.formatting.prettier" },
   {
     "neovim/nvim-lspconfig",
     opts = {
